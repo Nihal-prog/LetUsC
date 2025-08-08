c00886c49ce4d9bd58393c6dbcfaec8c099c6487
Nihal <nihalsingh010884@gmail.com>
2025-08-08 08:35:21 +0530

    Under Progress

---
+++ CH-03/ch03-04.c
@@ -0,0 +1,16 @@
+/* Take a year as integer input. And print what day it was on 1st Jan that year. */
+
+#include <stdio.h>
+
+int main()
+{
+    // Initialising Variables
+    int year;
+
+    // Taking input
+    printf("Enter the year\n");
+    scanf("%d", &year);
+
+    // Logic
+    return 0;
+}
